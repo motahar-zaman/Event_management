13051ed11ac17e416ae7a207242f99f48fcc8d8b
Motahar Zaman <motaharz95@gmail.com>
2020-12-02 12:53:30 +0600

    create migration for users, expenses and clients table

---
+++ database/migrations/2020_12_02_061549_create_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string("name",255);
+            $table->string("japanese_name",255)->nullable();
+            $table->integer("status");
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
